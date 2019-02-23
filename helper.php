3bb3b20fb49a39603485ed35a567e029c8a6f1b3
James Haggard <enquiries@jameshaggard.com>
2019-02-23 11:36:49 -0500

    Updated to use new asset URLs and support chorts.

--- helper.php
+++ helper.php
@@ -4,11 +4,14 @@ class ModFPBMHelper
     public static function getCatalog($params)
     {
         $providerID = $params->get('providerID');
+        $cohortID = $params->get('cohortID');
 
         $catalogHTML = <<<EOT
-<script src="https://probono.savvysuit.com/cdn-origin/webcomponents-lite.min.js"></script>
-<link rel="import" href="https://probono.savvysuit.com/cdn-origin/probono-catalog.html">
-<probono-catalog partner-id="$providerID"></probono-catalog>
+<probono-catalog cohort-id="$cohortID" partner-id="$providerID"></probono-catalog>
+<script src="https://probono.savvysuit.com/cdn-origin/current/webcomponents-loader.js"></script>
+<script type="module" src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog.js"></script>
+<script nomodule src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog-nomodule.js"></script>
+
 EOT;
 
         return $catalogHTML;
