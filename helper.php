0b0ce8936f32629021e85854123d24096e909199
James Haggard <enquiries@jameshaggard.com>
2019-06-22 09:46:40 -0400

    Updated to include dark mode option.

--- helper.php
+++ helper.php
@@ -5,9 +5,10 @@ class ModFPBMHelper
     {
         $providerID = $params->get('providerID');
         $cohortID = $params->get('cohortID');
+        $darkMode = $params->get('darkMode');
 
         $catalogHTML = <<<EOT
-<probono-catalog cohort-id="$cohortID" partner-id="$providerID"></probono-catalog>
+<probono-catalog cohort-id="$cohortID" partner-id="$providerID" dark-mode="$darkMode"></probono-catalog>
 <script src="https://probono.savvysuit.com/cdn-origin/current/webcomponents-loader.js"></script>
 <script type="module" src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog.js"></script>
 <script nomodule src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog-nomodule.js"></script>
