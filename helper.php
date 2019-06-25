6bf5fb1670dc5192229fc835b6b0d17a8083bc30
James Haggard <enquiries@jameshaggard.com>
2019-06-25 11:03:57 -0400

    Fixed partner/provider typo.

--- helper.php
+++ helper.php
@@ -8,7 +8,7 @@ class ModFPBMHelper
         $darkMode = $params->get('darkMode');
 
         $catalogHTML = <<<EOT
-<probono-catalog cohort-id="$cohortID" partner-id="$providerID" dark-mode="$darkMode"></probono-catalog>
+<probono-catalog cohort-id="$cohortID" partner-id="$partnerID" dark-mode="$darkMode"></probono-catalog>
 <script src="https://probono.savvysuit.com/cdn-origin/current/webcomponents-loader.js"></script>
 <script type="module" src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog.js"></script>
 <script nomodule src="https://probono.savvysuit.com/cdn-origin/current/probono-catalog-nomodule.js"></script>
